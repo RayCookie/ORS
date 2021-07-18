b82467f8421a8ba2c7b37064f8f676e8e68ec7c7
RayCookie <45881039+RayCookie@users.noreply.github.com>
2021-07-18 19:32:58 +0100

    Crud product test

--- ORS/routes/web.php
+++ ORS/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products','ProductController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
